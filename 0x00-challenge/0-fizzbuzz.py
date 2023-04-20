64c2151be4ffc70e5db6816a8e3190d7f2f1c0f7
DeenLennon <github.com/DeenLennon>
2023-04-20 18:33:07 +0100

    updated

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -3,36 +3,19 @@
 """
 import sys
 
-
-def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
-    if n < 1:
-        return
-
-    tmp_result = []
-    for i in range(1, 50):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
-        else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
-
-
-if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)
-
-    number = int(sys.argv[1])
-    fizzbuzz(number)
+# Author: Abu Abdulwahab aka Deen Lennon
+"""Print the numbers from 1 to 100 separated by a space.
+For multiples of three, print Fizz instead of the number
+For multiples of five, print Buzz instead of the number.
+For multiples of three and five, print FizzBuzz instead of the number.
+"""
+#def fizzbuzz():
+for number in range(1, 50):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz ", end="")
+    elif number % 3 == 0:
+        print("Fizz ", end="")
+    elif number % 5 == 0:
+        print("Buzz ", end="")
+    else:
+        print("{} ".format(number), end="")
